957589274909a1abef60f828c0bfa04d3ba6284f
@nazareno.molinari <nazareno.molinari@bitnovo.com>
2024-03-10 16:12:02 +0100

    fix get context type, no modal get context crash

--- packages/dependency/svelte/src/factorys/getModalContext.ts
+++ packages/dependency/svelte/src/factorys/getModalContext.ts
@@ -2,10 +2,14 @@ import type { ModalProps } from "@nazfy/modal"
 import { getContext } from "svelte"
 import { get, type Readable } from "svelte/store"
 
-export const getModalContext = () => {
+export const getModalContext = (): ModalProps & { store: Readable<ModalProps> } => {
     const store = getContext('modal_props_internal') as any
 
-    const { closeModal, ...otherProps } = get(store) as any
+    const getStore = get(store) as any
+
+    if(typeof getStore === 'undefined') return {} as any
+    
+    const { closeModal, ...otherProps } = getStore
 
     return {
         ...otherProps, closeModal: (...args: any[]) => (get(store) as any)?.closeModal?.(...args),
